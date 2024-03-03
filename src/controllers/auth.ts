b369bbb3af980da95c44f43c7800be07aeb61396
Prateek Chaurasia <prateek18801@gmail.com>
2024-03-04 02:05:41 +0530

    Create login controller

---
+++ src/controllers/auth.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/user";
+
+const postLogin = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (user && !user.archive) {
+            console.log(req.body);
+
+            if (await user.match(req.body.password)) {
+                const payload = {
+                    sub: user._id,
+                    role: user.role,
+                    name: user.name,
+                    username: user.username,
+                }
+                const token = jwt.sign(payload, process.env.JWT_SECRET as string, { expiresIn: "1d" });
+                return res.status(200).json({
+                    token,
+                    user: payload
+                });
+            }
+        }
+        return res.status(401).json({
+            message: "invalid credentials"
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+export {
+    postLogin
+}
