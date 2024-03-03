7ff0aed4f59bf6feba405980a7413cd2b6a67d82
Prateek Chaurasia <prateek18801@gmail.com>
2024-03-04 02:05:18 +0530

    Setup middlewares router and error controller

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
-import express, { Express } from "express";
+import express, { json, urlencoded, Express, Response, Request } from "express";
+import cors from "cors";
+import router from "./router";
+import db from "./utils/db";
 
 const app: Express = express();
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: false }));
 
-app.get("/ping", (_req, res) => {
+app.use(router);
+
+app.get("/ping", (_req: Request, res: Response) => {
     return res.status(200).json({
         message: "✅ server running"
     });
 });
 
-app.listen(3000, () => {
+app.use((err: Error, _req: Request, res: Response) => {
+    return res.status(500).json({
+        message: err.message
+    });
+});
+
+app.listen(process.env.PORT, () => {
     console.log("✅ server started");
+    db.connect();
 });
