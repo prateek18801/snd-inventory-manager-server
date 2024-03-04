aed6a59ac901613c850d1d2b40a8c89243723552
Prateek Chaurasia <prateek18801@gmail.com>
2024-03-05 02:34:21 +0530

    Setup multer add postProduct route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
+import multer, { memoryStorage } from "multer";
 import { postUsers } from "../controllers/user";
 import { postLogin } from "../controllers/auth";
+import { postProducts } from "../controllers/product";
 
 const router: Router = Router();
+const upload = multer({ storage: memoryStorage() });
 
 // auth routes
 router.post("/v1/login", postLogin);
 
 // user routes
 router.post("/v1/users", postUsers);
+
+// product routes
+router.post("/v1/products", upload.single("image"), postProducts);
 
 export default router;
