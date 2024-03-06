667582579a4d5b67b8ce8e8d4f87d7e90f6f5ad2
Prateek Chaurasia <prateek18801@gmail.com>
2024-03-07 04:31:04 +0530

    Implement postWarehouse controller

---
+++ src/controllers/warehouse.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from "express";
+import Warehouse from "../models/warehouse";
+
+const getWarehouses = async (req: Request, res: Response, next: NextFunction) => {
+
+}
+
+const postWarehouses = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        if (await Warehouse.findOne({ w_id: req.body.w_id })) {
+            return res.status(409).json({
+                message: "duplicate"
+            });
+        }
+        const warehouse = await new Warehouse(req.body).save();
+        return res.status(201).json({
+            message: "created",
+            data: warehouse
+        });
+    } catch(err) {
+        next(err);
+    }
+}
+
+const patchWarehouses = async (req: Request, res: Response, next: NextFunction) => {
+
+}
+
+const deleteWarehouses = async (req: Request, res: Response, next: NextFunction) => {
+
+}
+
+export {
+    getWarehouses,
+    postWarehouses,
+    patchWarehouses,
+    deleteWarehouses
+}
