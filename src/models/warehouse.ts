f2869540c742462b1640d3276a5198c25fb431d8
Prateek Chaurasia <prateek18801@gmail.com>
2024-03-07 04:30:32 +0530

    Remove IWarehouse interface

--- src/models/warehouse.ts
+++ src/models/warehouse.ts
@@ -1,12 +1,5 @@
 import { Schema, model } from "mongoose";
 
-interface IWarehouse {
-    w_id: string,
-    name: string,
-    location?: string,
-    description?: string
-};
-
 const WarehouseSchema = new Schema({
     w_id: { type: String, required: true, unique: true },
     name: { type: String, required: true },
@@ -14,4 +7,4 @@ const WarehouseSchema = new Schema({
     description: { type: String }
 }, { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } });
 
-export default model<IWarehouse>("Warehouse", WarehouseSchema);
+export default model("Warehouse", WarehouseSchema);
