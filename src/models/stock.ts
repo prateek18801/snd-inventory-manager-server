5045dab58d386f89b0b47ba0989baa1ea0ded7db
Prateek Chaurasia <prateek18801@gmail.com>
2024-03-07 04:32:39 +0530

    Make default quantity 0 remove IStock interface

--- src/models/stock.ts
+++ src/models/stock.ts
@@ -1,15 +1,9 @@
 import { Schema, model } from "mongoose";
 
-interface IStock {
-    product_id: Schema.Types.ObjectId,
-    warehouse_id: Schema.Types.ObjectId,
-    quantity: number
-};
-
 const StockSchema = new Schema({
     product_id: { type: Schema.Types.ObjectId, required: true, ref: "Product" },
     warehouse_id: { type: Schema.Types.ObjectId, required: true, ref: "Warehouse" },
-    quantity: { type: Number, required: true }
+    quantity: { type: Number, default: 0 }
 });
 
-export default model<IStock>("Stock", StockSchema);
+export default model("Stock", StockSchema);
