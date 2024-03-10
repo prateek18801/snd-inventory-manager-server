04995a8438799dcf4b312b75e0668e7aaa3f84e1
Prateek Chaurasia <prateek18801@gmail.com>
2024-03-10 23:35:26 +0530

    Add getAppContext controller

---
+++ src/controllers/page.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import Product from "../models/product";
+import Warehouse from "../models/warehouse";
+
+const getAppContext = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const product = await Product.find({}).select("_id p_id name").lean();
+        const warehouse = await Warehouse.find({}).select("_id w_id name").lean();
+        return res.status(200).json({
+            product: product,
+            warehouse: warehouse
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+export {
+    getAppContext
+}
