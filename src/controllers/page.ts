9e22b22c07a36f1b77ecc9821a50fca3e12bc8bb
Prateek Chaurasia <prateek18801@gmail.com>
2024-03-12 22:02:41 +0530

    Add image in populate product list

--- src/controllers/page.ts
+++ src/controllers/page.ts
@@ -4,8 +4,8 @@ import Warehouse from "../models/warehouse";
 
 const getAppContext = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const product = await Product.find({}).select("_id p_id name").lean();
-        const warehouse = await Warehouse.find({}).select("_id w_id name").lean();
+        const product = await Product.find({}).select("_id p_id name image").lean();
+        const warehouse = await Warehouse.find({}).select("_id w_id name").lean();        
         return res.status(200).json({
             product: product,
             warehouse: warehouse
