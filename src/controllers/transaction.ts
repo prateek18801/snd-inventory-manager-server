e06c0374b02a31b6728afc8078db72fd1b30453c
Prateek Chaurasia <prateek18801@gmail.com>
2024-03-14 20:38:42 +0530

    Implement sessions for atomic transactions

--- src/controllers/transaction.ts
+++ src/controllers/transaction.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { Types } from "mongoose";
+import { startSession, Types } from "mongoose";
 import Stock from "../models/stock";
 import Product from "../models/product";
 import Transaction from "../models/transaction";
@@ -11,43 +11,36 @@ const getTransactions = async (req: Request, res: Response, next: NextFunction) 
 const postTransactionsIn = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const transaction = new Transaction(req.body);
-        // TODO - transaction.user = new Types.ObjectId(req.user.sub);
+        transaction.action = "STOCK_IN";
+        // transaction.user = new Types.ObjectId(req.user.sub);
         transaction.user = new Types.ObjectId("65e4c6e211247715a07ead7e");
 
-        const product = await Product.findById(transaction.product);
-        const stock = await Stock.findOne({ product: transaction.product, warehouse: transaction.warehouse });
-        
-        if (!product) {
-            return res.status(400).json({
-                message: `Cannot find product with id="${transaction.product}"`
+        const session = await startSession();
+        session.startTransaction();
+        try {
+            await Product.findByIdAndUpdate(transaction.product, { $inc: { stock: transaction.quantity } }, { session });
+            await Stock.findOneAndUpdate({ product: transaction.product, warehouse: transaction.warehouse }, { $inc: { quantity: transaction.quantity } }, { upsert: true, session });
+            await transaction.save({ session });
+
+            await session.commitTransaction();
+            await session.endSession();
+
+            return res.status(201).json({
+                message: "Transaction successful",
+                data: transaction
             });
+        } catch (err) {
+            await session.abortTransaction();
+            await session.endSession();
+            throw err;
         }
-        product.stock = product.stock + transaction.quantity;
-
-        if(!stock) {
-            await new Stock({
-                product: transaction.product,
-                warehouse: transaction.warehouse,
-                quantity: transaction.quantity
-            }).save();
-        } else {
-            stock.quantity = stock.quantity + transaction.quantity;
-            await stock.save();
-        }
-
-        await product.save();
-        await transaction.save();
-        return res.status(201).json({
-            message: "Transaction successful",
-            data: transaction
-        });
     } catch (err) {
         next(err);
     }
 }
 
 const postTransactionsOut = async (req: Request, res: Response, next: NextFunction) => {
-
+    
 }
 
 export {
