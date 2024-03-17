6ab027aebbf110496aed534ef6d2477880c8de28
Prateek Chaurasia <prateek18801@gmail.com>
2024-03-18 00:41:34 +0530

    Fix empty warehouse allocation refactor code

--- src/controllers/stock.ts
+++ src/controllers/stock.ts
@@ -22,14 +22,16 @@ const getWarehouseBreakdown = async (req: Request, res: Response, next: NextFunc
                     warehouse: ws.warehouse,
                     quantity: ws.quantity
                 });
-            } else {
+                continue;
+            }
+            if (required > 0) {
                 breakdown.push({
                     warehouse: ws.warehouse,
                     quantity: required
                 });
                 required = 0;
-                break;
             }
+            break;
         }
         if (required) return res.status(400).json({
             message: `Requested quantity ${req.query.quantity} is greater than available quantity ${+(req.query.quantity || 0) - required}`
