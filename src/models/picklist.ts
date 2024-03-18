0e55e52bb006bbf74b368e96880ee68e053f1ac7
Prateek Chaurasia <prateek18801@gmail.com>
2024-03-19 02:50:27 +0530

    Create picklist schema

---
+++ src/models/picklist.ts
@@ -0,0 +1,14 @@
+import { Schema, model } from "mongoose";
+
+const PicklistSchema = new Schema({
+    list: [{
+        product: { type: Schema.Types.ObjectId, required: true, ref: "Product" },
+        quantity: { type: Number, required: true },
+        breakdown: [{
+            warehouse: { type: Schema.Types.ObjectId, required: true, ref: "Warehouse" },
+            quantity: { type: Number, required: true }
+        }]
+    }]
+}, { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } });
+
+export default model("Picklist", PicklistSchema);
