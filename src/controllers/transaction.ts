ea34d2a16cc924b1004127e71f4211321962ffe5
Prateek Chaurasia <prateek18801@gmail.com>
2024-03-18 23:17:46 +0530

    Implement multiple transaction processing (for picklist)

--- src/controllers/transaction.ts
+++ src/controllers/transaction.ts
@@ -41,10 +41,81 @@ const postTransactionsIn = async (req: Request, res: Response, next: NextFunctio
 
 const postTransactionsOut = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        // for processing multiple transactions (picklist)
         if (Array.isArray(req.body)) {
-            // process for picklist
+            const result = [];
+            for (const item of req.body) {
+                const stocks = await Stock.find({ product: item.product }).populate("warehouse").sort({ quantity: 1 });
+                if (item.quantity > stocks.reduce((total, { quantity }) => total + quantity, 0)) continue;
+
+                let required: number = item.quantity;
+                const updates = [];
+                const transactions = [];
+
+                // for result
+                const breakdown = [];
+                for (const stock of stocks) {
+                    if (stock.quantity <= required) {
+                        transactions.push(new Transaction({
+                            action: "STOCK_OUT",
+                            reason: "Outbound",
+                            quantity: stock.quantity,
+                            // user: new new Types.ObjectId(req.user.sub),
+                            user: new Types.ObjectId("65e4c6e211247715a07ead7e"),
+                            product: stock.product,
+                            warehouse: stock.warehouse._id
+                        }).save());
+
+                        // for result
+                        breakdown.push({
+                            warehouse: stock.warehouse,
+                            quantity: stock.quantity
+                        });
+
+                        required -= stock.quantity;
+                        stock.quantity = 0;
+                        updates.push(stock.save());
+                        continue;
+                    }
+                    if (required > 0) {
+                        transactions.push(new Transaction({
+                            action: "STOCK_OUT",
+                            reason: "Outbound",
+                            quantity: required,
+                            // user: new new Types.ObjectId(req.user.sub),
+                            user: new Types.ObjectId("65e4c6e211247715a07ead7e"),
+                            product: stock.product,
+                            warehouse: stock.warehouse._id
+                        }).save());
+
+                        // for result
+                        breakdown.push({
+                            warehouse: stock.warehouse,
+                            quantity: required
+                        });
+
+                        stock.quantity -= required;
+                        updates.push(stock.save());
+                        required = 0;
+                    }
+                    break;
+                }
+                await Promise.all(updates);
+                await Promise.all(transactions);
+                const product = await Product.findByIdAndUpdate(item.product, { $inc: { stock: -item.quantity } }, { new: true });
+                result.push({
+                    product: product,
+                    quantity: item.quantity,
+                    breakdown: breakdown
+                });
+            }
+            return res.status(201).json({
+                message: "Transaction Successful",
+                data: result
+            });
         }
 
+        // for processing single transaction 
         const transaction = new Transaction(req.body);
         transaction.action = "STOCK_OUT";
         // transaction.user = new Types.ObjectId(req.user.sub);
@@ -60,7 +131,7 @@ const postTransactionsOut = async (req: Request, res: Response, next: NextFuncti
                     message: `Requested quantity ${transaction.quantity} is greater than warehouse quantity ${stock?.quantity || 0}`
                 });
             }
-            
+
             await Product.findByIdAndUpdate(transaction.product, { $inc: { stock: -transaction.quantity } }, { session });
             await Stock.findOneAndUpdate({ product: transaction.product, warehouse: transaction.warehouse }, { $inc: { quantity: -transaction.quantity } }, { session });
             await transaction.save({ session });
