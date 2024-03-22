e750af8702353c61555c540d04d5287464385d77
Prateek Chaurasia <prateek18801@gmail.com>
2024-03-22 15:02:57 +0530

    Implement delete controllers

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import User from "../models/user";
 
 const getUsers = async (req: Request, res: Response, next: NextFunction) => {
-    
+
 }
 
 const postUsers = async (req: Request, res: Response, next: NextFunction) => {
@@ -22,11 +22,21 @@ const patchUsers = async (req: Request, res: Response, next: NextFunction) => {
 }
 
 const softDeleteUsers = async (req: Request, res: Response, next: NextFunction) => {
-
+    try {
+        await User.findByIdAndUpdate(req.params.id, { archive: true });
+        return res.status(204).json();
+    } catch (err) {
+        next(err);
+    }
 }
 
 const hardDeleteUsers = async (req: Request, res: Response, next: NextFunction) => {
-
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        return res.status(204).json();
+    } catch (err) {
+        next(err);
+    }
 }
 
 export {
