f1c581001283589298caed783748b6bd71ddc1e6
Prateek Chaurasia <prateek18801@gmail.com>
2024-03-30 10:04:39 +0530

    Implement auth middleware

--- src/router/index.ts
+++ src/router/index.ts
@@ -30,6 +30,7 @@ import {
     exportTransactions
 } from "../controllers/transaction";
 import { getAppContext, getDashboard } from "../controllers/page";
+import auth from "../middleware/auth";
 
 const router: Router = Router();
 const upload = multer({ storage: memoryStorage() });
@@ -38,35 +39,35 @@ const upload = multer({ storage: memoryStorage() });
 router.post("/v1/login", postLogin);
 
 // user routes
-router.post("/v1/users", postUsers);
-router.delete("/v1/users/:id", softDeleteUsers);
-router.delete("/v1/users/hard/:id", hardDeleteUsers);
+router.post("/v1/users", auth("root"), postUsers);
+router.delete("/v1/users/:id", auth("root"), softDeleteUsers);
+router.delete("/v1/users/hard/:id", auth("root"), hardDeleteUsers);
 
 // product routes
-router.get("/v1/products", getProducts);
-router.post("/v1/products", upload.single("image"), postProducts);
-router.patch("/v1/products/:id", upload.single("image"), patchProducts);
-router.delete("/v1/products/:id", deleteProducts);
-router.get("/v1/product/export", exportProducts);
+router.get("/v1/products", auth("manager"), getProducts);
+router.post("/v1/products", auth("manager"), upload.single("image"), postProducts);
+router.patch("/v1/products/:id", auth("manager"), upload.single("image"), patchProducts);
+router.delete("/v1/products/:id", auth("manager"), deleteProducts);
+router.get("/v1/product/export", auth("manager"), exportProducts);
 
 // warehouse routes
-router.get("/v1/warehouses", getWarehouses);
-router.post("/v1/warehouses", postWarehouses);
-router.delete("/v1/warehouses/:id", deleteWarehouses);
+router.get("/v1/warehouses", auth("manager"), getWarehouses);
+router.post("/v1/warehouses", auth("manager"), postWarehouses);
+router.delete("/v1/warehouses/:id", auth("manager"), deleteWarehouses);
 
 // stock routes
-router.get("/v1/stocks", getStocks);
-router.get("/v1/breakdown", getWarehouseBreakdown);
-router.get("/v1/stocks/export", exportStocks);
+router.get("/v1/stocks", auth("executive"), getStocks);
+router.get("/v1/breakdown", auth("executive"), getWarehouseBreakdown);
+router.get("/v1/stocks/export", auth("executive"), exportStocks);
 
 // transaction routes
-router.get("/v1/transactions", getTransactions);
-router.post("/v1/transactions/in", postTransactionsIn);
-router.post("/v1/transactions/out", postTransactionsOut);
-router.get("/v1/transactions/export", exportTransactions);
+router.get("/v1/transactions", auth("manager"), getTransactions);
+router.post("/v1/transactions/in", auth("executive"), postTransactionsIn);
+router.post("/v1/transactions/out", auth("executive"), postTransactionsOut);
+router.get("/v1/transactions/export", auth("manager"), exportTransactions);
 
 // application routes
-router.get("/v1/app/context", getAppContext);
-router.get("/v1/app/dashboard", getDashboard);
+router.get("/v1/app/context", auth("executive"), getAppContext);
+router.get("/v1/app/dashboard", auth("manager"), getDashboard);
 
 export default router;
