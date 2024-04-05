2ab3df6606ee247da44380242a243026acd783c2
Prateek Chaurasia <prateek18801@gmail.com>
2024-04-06 00:19:33 +0530

    Add lead_time and drr fields

--- src/models/product.ts
+++ src/models/product.ts
@@ -3,8 +3,9 @@ import { Schema, model } from "mongoose";
 const ProductSchema = new Schema({
     p_id: { type: String, required: true, unique: true },
     name: { type: String, required: true },
-    alert: { type: Number, required: true },
     stock: { type: Number, default: 0 },
+    lead_time: { type: Number, required: true },
+    drr: { type: Number, default: 0 },
     image: { type: String },
     description: { type: String }
 }, { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } });
