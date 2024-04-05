a0ec2cc2778f70a9c12dbb4f51015715ad8a00cf
Prateek Chaurasia <prateek18801@gmail.com>
2024-04-06 01:09:33 +0530

    Remove description field

--- src/models/product.ts
+++ src/models/product.ts
@@ -6,8 +6,7 @@ const ProductSchema = new Schema({
     stock: { type: Number, default: 0 },
     lead_time: { type: Number, required: true },
     drr: { type: Number, default: 0 },
-    image: { type: String },
-    description: { type: String }
+    image: { type: String }
 }, { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } });
 
 export default model("Product", ProductSchema);
