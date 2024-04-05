417a23291c073a130e1843d8c0e1db8e03ca527c
Prateek Chaurasia <prateek18801@gmail.com>
2024-04-05 12:13:17 +0530

    Implement cascade delete transactions on warehouse deletion

--- src/controllers/warehouse.ts
+++ src/controllers/warehouse.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import Stock from "../models/stock";
 import Warehouse from "../models/warehouse";
+import Transaction from "../models/transaction";
 
 const getWarehouses = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -33,6 +34,7 @@ const deleteWarehouses = async (req: Request, res: Response, next: NextFunction)
         const deleted = await Warehouse.findByIdAndDelete(req.params.id);
         if (deleted) {
             await Stock.deleteMany({ warehouse: deleted._id });
+            await Transaction.deleteMany({ product: deleted._id });
         }
         return res.status(204).json();
     } catch (err) {
