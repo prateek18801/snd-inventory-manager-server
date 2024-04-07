e5bc32e427c34bd79746b2effb05bd7b01bf839b
Prateek Chaurasia <prateek18801@gmail.com>
2024-04-07 23:08:44 +0530

    Implement low stock in getDashboard refactor existing code for redundant loops

--- src/controllers/page.ts
+++ src/controllers/page.ts
@@ -19,47 +19,67 @@ const getAppContext = async (req: Request, res: Response, next: NextFunction) =>
 
 const getDashboard = async (req: Request, res: Response, next: NextFunction) => {
     try {
+        // set dashboard data start and end range
         const today = new Date();
-        const start = `${req.query.start || new Date(today.setDate(today.getDate() - 1)).toISOString().split("T")[0]}T00:00:00.000+05:30`;
-        const end = `${req.query.end || new Date(today.setDate(today.getDate() - 1)).toISOString().split("T")[0]}T23:59:59.999+05:30`;
+        const start = `${req.query.start || new Date(today.setDate(today.getDate() - 1)).toLocaleDateString("fr-CA")}T00:00:00.000+05:30`;
+        const end = `${req.query.end || new Date(today.setDate(today.getDate() - 1)).toLocaleDateString("fr-CA")}T23:59:59.999+05:30`;
 
+        // find sales transactions in the date range
         const transactions = await Transaction.find({
             created_at: { $gte: start, $lte: end },
             action: "STOCK_OUT",
             reason: "picklist"
         }).lean();
 
-        const frequency: Record<string, number> = {};
+        // calculate count of all products sold
+        const productSalesFrequency: Record<string, number> = {};
         for (const transaction of transactions) {
-            if (!frequency[transaction.product.toString()]) {
-                frequency[transaction.product.toString()] = transaction.quantity;
+            if (!productSalesFrequency[transaction.product.toString()]) {
+                productSalesFrequency[transaction.product.toString()] = transaction.quantity;
                 continue;
             }
-            frequency[transaction.product.toString()] += transaction.quantity;
+            productSalesFrequency[transaction.product.toString()] += transaction.quantity;
         }
 
+        // find difference in days
         const period = Math.ceil((new Date(end).getTime() - new Date(start).getTime()) / (1000 * 60 * 60 * 24));
 
-        const products = await Product.find({ _id: { $in: Object.keys(frequency) } }).lean();
-        const trending = products.map(product => ({
-            ...product,
-            drr: Math.round((frequency[product._id.toString()] / period) * 10) / 10
-        })).sort((a, b) => b.drr - a.drr);
+        // calculate drr for products sold in the date range
+        const products = await Product.find({}).lean();
+        const trendingProducts = [];
+        const lowStockProducts = [];
 
+        for (const product of products) {
+            if (product._id.toString() in productSalesFrequency) {
+                trendingProducts.push({
+                    ...product,
+                    drr: +(productSalesFrequency[product._id.toString()] / period).toFixed(1)
+                });
+            }
+            if (product.stock && product.stock <= Math.ceil(product.drr * product.lead_time)) {
+                lowStockProducts.push(product);
+            }
+        }
+
+        trendingProducts.sort((a, b) => b.drr - a.drr);
+        lowStockProducts.sort((a, b) => (Math.ceil(b.drr * b.lead_time) - b.stock) - (Math.ceil(a.drr * a.lead_time) - a.stock));
+
+        // calculate channel distribution for sales
         const picklists = await Picklist.find({ created_at: { $gte: start, $lte: end } }).lean();
-
-        const distribution: Record<string, number> = {};
+        const channelDistribution: Record<string, number> = {};
         for (const picklist of picklists) {
             const totalQty = picklist.list.reduce((acc, obj) => acc + obj.quantity, 0);
-            if (!distribution[picklist.channel]) {
-                distribution[picklist.channel] = totalQty;
+            if (!channelDistribution[picklist.channel]) {
+                channelDistribution[picklist.channel] = totalQty;
                 continue;
             }
-            distribution[picklist.channel] += totalQty;
+            channelDistribution[picklist.channel] += totalQty;
         }
+
         return res.status(200).json({
-            trending,
-            distribution
+            trending_products: trendingProducts,
+            channel_distribution: channelDistribution,
+            low_stock_products: lowStockProducts
         });
     } catch (err) {
         next(err);
