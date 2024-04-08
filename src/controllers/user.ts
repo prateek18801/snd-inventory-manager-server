bcc53b53798aa8b80e0406aec2c4984011b29b70
Prateek Chaurasia <prateek18801@gmail.com>
2024-04-08 16:53:51 +0530

    Update patchUsers handle password update

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/user";
 
-const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+const getUsers = async (_req: Request, res: Response, next: NextFunction) => {
     try {
         const users = await User.find({ archived: false }).select({ password: 0 }).lean();
         return res.status(200).json(users);
@@ -24,16 +24,21 @@ const postUsers = async (req: Request, res: Response, next: NextFunction) => {
 
 const patchUsers = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const updated = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if(!updated) {
+        const user = await User.findById(req.params.id);
+        if (!user) {
             return res.status(400).json({
                 message: "user not found"
             });
         }
-        const { password, ...user } = updated;
+        Object.keys(req.body).forEach(key => { if (req.body[key]) (user as any)[key] = req.body[key] });
+        const updated = await user.save();
         return res.status(200).json({
-            message: "updated",
-            data: user
+            message: "user updated",
+            data: {
+                name: updated.name,
+                role: updated.role,
+                username: updated.username
+            }
         });
     } catch (err) {
         next(err);
@@ -43,7 +48,7 @@ const patchUsers = async (req: Request, res: Response, next: NextFunction) => {
 
 const softDeleteUsers = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        await User.findByIdAndUpdate(req.params.id, { archive: true });
+        await User.findByIdAndUpdate(req.params.id, { archived: true });
         return res.status(204).json();
     } catch (err) {
         next(err);
