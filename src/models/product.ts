e27faed1a56c352fa4f75947fb372204bc72a9db
Prateek Chaurasia <prateek18801@gmail.com>
2024-05-07 17:58:24 +0530

    Add in_transit stock to product schema

--- src/models/product.ts
+++ src/models/product.ts
@@ -4,6 +4,7 @@ const ProductSchema = new Schema({
     p_id: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     stock: { type: Number, default: 0 },
+    in_transit: { type: Number, default: 0 },
     lead_time: { type: Number, required: true },
     drr: { type: Number, default: 0 },
     image: { type: String }
