c1a77b2a3c8cf2f4290983f5444e44cafbec9923
Prateek Chaurasia <prateek18801@gmail.com>
2024-05-07 17:57:40 +0530

    Add shipment model

---
+++ src/models/shipment.ts
@@ -0,0 +1,12 @@
+import { Schema, model } from "mongoose";
+
+const ShipmentSchema = new Schema({
+    container: { type: String, required: true },
+    list: [{
+        product: { type: Schema.Types.ObjectId, required: true, ref: "Product" },
+        quantity: { type: Number, required: true }
+    }],
+    received: { type: Boolean, default: false }
+}, { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } });
+
+export default model("Shipment", ShipmentSchema);
