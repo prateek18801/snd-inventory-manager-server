bbf61b469212914cccd7d745b8cc13787e3af8cc
Prateek Chaurasia <prateek18801@gmail.com>
2024-05-09 11:34:30 +0530

    Add expected_delivery field in shipment

--- src/models/shipment.ts
+++ src/models/shipment.ts
@@ -6,7 +6,8 @@ const ShipmentSchema = new Schema({
         product: { type: Schema.Types.ObjectId, required: true, ref: "Product" },
         quantity: { type: Number, required: true }
     }],
-    received: { type: Boolean, default: false }
+    received: { type: Boolean, default: false },
+    expected_delivery: { type: Date }
 }, { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } });
 
 export default model("Shipment", ShipmentSchema);
